524c1753bd24ed927724045d374d2ff5f5109004
penglei <yupengniaosanshi@gmail.com>
2019-10-02 15:17:24 +0800

    init

---
+++ src/com/company/Alphabet.java
@@ -0,0 +1,39 @@
+package com.company;
+
+/**
+ * 字母表，一个很有用的数据结构，但是目前仅使用字符串
+ */
+public class Alphabet {
+    public Alphabet(String s) {
+    }
+
+    char toChar(int index) {
+        return 0;
+    }
+
+    int toIndex(char c) {
+        return -1;
+    }
+
+    boolean contains(char c) {
+        return false;
+    }
+
+    // 字符数
+    int R() {
+        return 0;
+    }
+
+    // 表示字符数所需的比特数
+    int lgR() {
+        return 0;
+    }
+
+    int[] toIndices(String s) {
+        return new int[0];
+    }
+
+    String toChars(int[] indices) {
+        return "";
+    }
+}
