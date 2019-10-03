4dcbe487d6ddf9b64ede030493568d8c3a418b72
penglei <yupengniaosanshi@gmail.com>
2019-10-03 22:05:04 +0800

    5.2 单词查找树	字符串符号表API

---
+++ src/com/company/ch5_2_单词查找树/StringST.java
@@ -0,0 +1,27 @@
+package com.company.ch5_2_单词查找树;
+
+/**
+ * 以字符串为键的符号表API
+ */
+public interface StringST<Value> {
+
+    void put(String key, Value value);
+
+    Value get(String key);
+
+    void delete(String key);
+
+    boolean contains(String key);
+
+    boolean isEmpty();
+
+    String longestPrefixOf(String s);
+
+    Iterable<String> keysWithPrefix(String s);
+
+    Iterable<String> keysThatMatch(String s);
+
+    int size();
+
+    Iterable<String> keys();
+}
